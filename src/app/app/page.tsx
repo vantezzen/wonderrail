c6eab0a28a42794dd87f8d9725fe957bad2822cc
Bennett <hello@vantezzen.io>
2023-06-16 19:20:41 +0200

    dev

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -15,61 +15,69 @@ function AppPage() {
 
   return (
     <div className="p-6 md:p-12">
-      <Card>
-        <CardHeader>
-          <div className="flex justify-between gap-4 items-center">
-            <CardTitle>Your journeys</CardTitle>
-            <Link href="/journeys/new">
-              <Button>Create new journey</Button>
-            </Link>
-          </div>
-        </CardHeader>
-        <CardContent>
-          {journeys?.size === 0 && !isLoading && (
-            <div className="mt-6 text-center">
-              <h3 className="text-xl font-medium">No journeys yet</h3>
-              <p className="text-zinc-500 font-medium">
-                Add your first journey by clicking the button above
-              </p>
-            </div>
-          )}
-
-          {journeys?.docs.map((journeyItem) => {
-            const journey = JSON.parse(journeyItem.data().journey) as Journey;
-
-            return (
-              <Link href={`/journeys/${user?.uid}/${journeyItem.id}`}>
-                <button className="rounded bg-zinc-800 hover:bg-zinc-900 p-6 w-full text-left duration-100">
-                  <h3 className="text-lg font-bold">{journey.name}</h3>
-
-                  <p className="text-zinc-500 font-medium mt-2">
-                    {journey.description}
-                  </p>
-
-                  <div className="flex gap-1 items-center text-zinc-500 text-sm mt-2">
-                    <span>
-                      {
-                        journey.steps.filter((step) => step.type === "stay")
-                          .length
-                      }{" "}
-                      stops
-                    </span>
-
-                    <Dot size={12} className="text-zinc-500" />
-
-                    <span>
-                      Starting{" "}
-                      {new Date(
-                        journey.startDate as unknown as string
-                      ).toLocaleDateString()}
-                    </span>
-                  </div>
-                </button>
+      <div className="grid grid-cols-3 gap-12">
+        <Card className="col-span-2">
+          <CardHeader>
+            <div className="flex justify-between gap-4 items-center">
+              <CardTitle>Your journeys</CardTitle>
+              <Link href="/journeys/new">
+                <Button>Create new journey</Button>
               </Link>
-            );
-          })}
-        </CardContent>
-      </Card>
+            </div>
+          </CardHeader>
+          <CardContent>
+            {journeys?.size === 0 && !isLoading && (
+              <div className="mt-6 text-center">
+                <h3 className="text-xl font-medium">No journeys yet</h3>
+                <p className="text-zinc-500 font-medium">
+                  Add your first journey by clicking the button above
+                </p>
+              </div>
+            )}
+
+            {journeys?.docs.map((journeyItem) => {
+              const journey = JSON.parse(journeyItem.data().journey) as Journey;
+
+              return (
+                <Link href={`/journeys/${user?.uid}/${journeyItem.id}`}>
+                  <button className="rounded bg-zinc-800 hover:bg-zinc-900 p-6 w-full text-left duration-100">
+                    <h3 className="text-lg font-bold">{journey.name}</h3>
+
+                    <p className="text-zinc-500 font-medium mt-2">
+                      {journey.description}
+                    </p>
+
+                    <div className="flex gap-1 items-center text-zinc-500 text-sm mt-2">
+                      <span>
+                        {
+                          journey.steps.filter((step) => step.type === "stay")
+                            .length
+                        }{" "}
+                        stops
+                      </span>
+
+                      <Dot size={12} className="text-zinc-500" />
+
+                      <span>
+                        Starting{" "}
+                        {new Date(
+                          journey.startDate as unknown as string
+                        ).toLocaleDateString()}
+                      </span>
+                    </div>
+                  </button>
+                </Link>
+              );
+            })}
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>My profile</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
     </div>
   );
 }
