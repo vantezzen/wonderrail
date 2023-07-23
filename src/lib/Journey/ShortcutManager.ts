b5d55699f2e91cf3b14144edb7649cb28a64af90
Bennett <hello@vantezzen.io>
2023-07-23 20:31:29 +0200

    Fix shortcuts not preventing default

--- src/lib/Journey/ShortcutManager.ts
+++ src/lib/Journey/ShortcutManager.ts
@@ -12,10 +12,20 @@ export default class ShortcutManager extends EventEmitter {
     if (hasCtrl && e.shiftKey && e.key.length === 1) {
       trackEvent(`shortcut_ctrl_shift_${e.key}`);
       this.emit(`ctrl-shift-${e.key}`);
+      if (this.isShortcutRegistered(`ctrl-shift-${e.key}`)) {
+        e.preventDefault();
+      }
     } else if (hasCtrl && e.key.length === 1) {
       trackEvent(`shortcut_ctrl_${e.key}`);
       this.emit(`ctrl-${e.key}`);
+      if (this.isShortcutRegistered(`ctrl-${e.key}`)) {
+        e.preventDefault();
+      }
     }
     return true;
   };
+
+  isShortcutRegistered(shortcut: string) {
+    return this.eventNames().includes(shortcut);
+  }
 }
