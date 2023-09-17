40b8989b534c56f3b8292f51644d4e6d25f65c54
Bennett <hello@vantezzen.io>
2023-09-17 11:36:58 +0200

    Update login design

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,6 +7,7 @@ import logoImage from "@/assets/logo.png";
 import Image from "next/image";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
+import "./firebase.css";
 
 const uiConfig: firebaseui.auth.Config = {
   signInSuccessUrl: "/app",
@@ -43,22 +44,55 @@ function SignInScreen() {
   }, [user]);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center gap-4 bg-zinc-900">
-      <Image src={logoImage} width={150} height={150} alt="WonderRail logo" />
+    <div className="min-h-screen flex flex-col lg:grid lg:grid-cols-2 gap-4">
+      <div className="lg:h-screen p-6">
+        <div className="h-full w-full rounded-xl bg-zinc-900 text-white p-16">
+          <Image
+            src={logoImage}
+            width={100}
+            height={100}
+            alt="WonderRail logo"
+          />
 
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseAuth} />
+          <h2 className="hidden lg:block lg:text-5xl font-extrabold max-w-2xl mt-12">
+            Discover the smartest way to{" "}
+            <span
+              style={{
+                background: "linear-gradient(132deg, #30A76D 0%, #295F45 100%)",
+                backgroundClip: "text",
+                // @ts-ignore
+                "-webkit-background-clip": "text",
+                // @ts-ignore
+                "-webkit-text-fill-color": "transparent",
+              }}
+            >
+              Interrail
+            </span>
+          </h2>
 
-      <p className="mt-6 font-medium text-zinc-500 text-center">
-        By continuing, you agree to WonderRail&apos;s{" "}
-        <Link href="/legal/terms" className="underline">
-          Terms of Service
-        </Link>{" "}
-        and{" "}
-        <Link href="/legal/privacy" className="underline">
-          Privacy Policy
-        </Link>
-        .
-      </p>
+          <p className="text-xl mt-8 text-zinc-400">
+            You can use the WonderRail planner without logging in - create a
+            free account to save your journeys in the cloud and share them with
+            friends.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-4 p-6 max-w-xl mx-auto">
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseAuth} />
+
+        <p className="font-medium text-zinc-500 text-center">
+          By continuing, you agree to WonderRail&apos;s{" "}
+          <Link href="/legal/terms" className="underline">
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link href="/legal/privacy" className="underline">
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
     </div>
   );
 }
